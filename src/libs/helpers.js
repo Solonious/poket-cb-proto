24fd3ef8803b627671f530f48e2e090d3159a0df
Sergii Solonar <sergeysolonar@gmail.com>
2017-04-02 20:32:30 +0300

    code refactored

--- src/libs/helpers.js
+++ src/libs/helpers.js
@@ -1,16 +1,3 @@
 export const YEAR = function getYear() {
     return new Date().getFullYear();
 }();
-
-export const validate = (values, requiredFields) => {
-	const errors = {};
-	requiredFields.forEach(field => {
-		if (!values[ field ]) {
-			errors[ field ] = 'Required'
-		}
-	})
-	if (values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-		errors.email = 'Invalid email address'
-	}
-	return errors
-};
